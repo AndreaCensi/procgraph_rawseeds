0fb5ece179ee681a250cb1c368b35b5926fa8738
Andrea Censi <andrea@cds.caltech.edu>
2010-12-01 22:58:20 -0800

    shortcuts

--- src/procgraph_rawseeds/__init__.py
+++ src/procgraph_rawseeds/__init__.py
@@ -10,14 +10,5 @@ import rawseeds_gps
 __version__ = "1.0" 
 
 
-
-# FIXME, make this easier
-import os
-from procgraph.core.registrar import default_library
-from procgraph.core.model_loader import pg_look_for_models
-dir = os.path.join(os.path.dirname(__file__), 'models')
-pg_look_for_models(default_library, additional_paths=[dir], ignore_env=True,
-                   assign_to_module='procgraph_rawseeds')
-
-
-
+from procgraph import pg_add_this_package_models
+pg_add_this_package_models(file=__file__, assign_to=__package__)
