59f6578d1aa080144d635b080a5cbf24aca6dbe5
Andrea Censi <andrea@cds.caltech.edu>
2010-12-05 14:14:01 -0800

    cosmetics

--- src/procgraph_rawseeds/__init__.py
+++ src/procgraph_rawseeds/__init__.py
@@ -7,8 +7,5 @@ import rawseeds_hokuyo
 import rawseeds_gps
 
 
-__version__ = "1.0" 
-
-
 from procgraph import pg_add_this_package_models
 pg_add_this_package_models(file=__file__, assign_to=__package__)
